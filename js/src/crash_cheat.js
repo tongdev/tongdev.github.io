f913e67bd0d625b319a0ce67e16395ab5d46e345
tongdev <1336572985@qq.com>
2018-11-21 20:47:44 +0800

    Site updated: 2018-11-21 20:47:43

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -2,19 +2,19 @@
 * @Author: tongdev
 * @Date:   2018-11-20 19:32:25
 * @Last Modified by:   tongdev
-* @Last Modified time: 2018-11-20 19:59:42
+* @Last Modified time: 2018-11-21 20:47:12
 */
 <!--崩溃欺骗-->
  var OriginTitle = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/images/favicon.ico");
+         $('[rel="icon"]').attr('href', "/images/favicon1.ico");
          document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/images/favicon.ico");
+         $('[rel="icon"]').attr('href', "/images/favicon1.ico");
          document.title = '我们又见面了~' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
